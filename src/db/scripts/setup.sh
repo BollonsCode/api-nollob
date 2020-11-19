7734e5624badd2b3f82af8d58f125d3cab2bd556
Victor Geruso <victor.geruso@gmail.com>
2020-11-18 22:08:34 -0300

    [UPD] Ajuste de conexao ao atlas e criação descript de build

--- src/db/scripts/setup.sh
+++ src/db/scripts/setup.sh
@@ -10,7 +10,7 @@ echo -e "import mongoose from 'mongoose'\n\n" >> database.js
 
 echo -e "const connection = {\n" >> database.js
 echo -e "  con() {\n" >> database.js
-echo -e "    return mongoose.connect('mongodb://nollobbot:ph]K,w63[H@mongo_nollob_db:27017/nollob_db', { useNewUrlParser: true, useUnifiedTopology: true });" >> database.js
+echo -e "    return mongoose.connect('mongodb+srv://nollobBot:7TlIshfk3oQbjOwQ@cluster0.qrxol.mongodb.net/nollob_db?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });" >> database.js
 echo -e "  }" >> database.js
 echo -e "}\n\n" >> database.js
 
