484e4fe05ab0f89fef4fc62b79d967a412be1fd8
Victor Geruso <victor.geruso@gmail.com>
2020-11-19 11:31:37 -0300

    [UPD] PORT

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import App from './App';
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3000;
 
 App.listen(PORT, () => {
   console.log('Server OK!'); // eslint-disable-line
