63832c2f3db51b7f8634a2713461f3dbdb1ddc54
Victor Geruso <victor.geruso@gmail.com>
2020-11-18 21:16:42 -0300

    [UPD] ajuste do arquivo de setup para produção

--- src/db/scripts/setup.sh
+++ src/db/scripts/setup.sh
@@ -10,7 +10,7 @@ echo -e "import mongoose from 'mongoose'\n\n" >> database.js
 
 echo -e "const connection = {\n" >> database.js
 echo -e "  con() {\n" >> database.js
-echo -e "    return mongoose.connect('mongodb://localhost:27017/nollob_dev_db', { useNewUrlParser: true, useUnifiedTopology: true });" >> database.js
+echo -e "    return mongoose.connect('mongodb://nollobbot:ph]K,w63[H@mongo_nollob_db:27017/nollob_db', { useNewUrlParser: true, useUnifiedTopology: true });" >> database.js
 echo -e "  }" >> database.js
 echo -e "}\n\n" >> database.js
 
